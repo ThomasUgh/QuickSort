6e0f65cbd817cc196b257a6b540baa462df87786
Thomas U. <Info@ThomasU.dev>
2024-08-28 00:11:06 +0200

    Add CategorizedCharacters Function

--- QuickSort/src/StringCategorizer.java
+++ QuickSort/src/StringCategorizer.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCategorizer {
+
+    public CategorizedCharacters categorize(String input) {
+        List<Character> lowercase = new ArrayList<>();
+        List<Character> uppercase = new ArrayList<>();
+        List<Character> digits = new ArrayList<>();
+        List<Character> specialCharacters = new ArrayList<>();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                lowercase.add(c);
+            } else if (Character.isUpperCase(c)) {
+                uppercase.add(c);
+            } else if (Character.isDigit(c)) {
+                digits.add(c);
+            } else {
+                specialCharacters.add(c);
+            }
+        }
+    }
+    }
 }
